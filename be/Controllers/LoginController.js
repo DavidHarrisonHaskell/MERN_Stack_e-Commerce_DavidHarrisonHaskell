cf187e43f1cb7b18ef089a7e29b4a1634867269a
David Harrison Haskell <david.h.haskell@gmail.com>
2024-09-23 14:41:01 +0300

    changes

--- be/Controllers/LoginController.js
+++ be/Controllers/LoginController.js
@@ -11,7 +11,7 @@ Router.post('/login', async (req, res) => {
         const Username = req.body.Username;
         const Password = req.body.Password;
         if (!Username || !Password) {
-            return res.status(400).json({ error: 'Please enter username and password' });
+            return res.status(400).json({ success: false, error: 'Please enter username and password' });
         }
         const users = await UsersService.getUsersService();
         const user = users.find(user => user.Username === Username && user.Password === Password);
@@ -20,9 +20,9 @@ Router.post('/login', async (req, res) => {
         }
         const secretKey = user.admin ? process.env.ADMIN_SECRET_KEY : process.env.USER_SECRET_KEY; // Use different secret keys for admin and user
         const token = jwt.sign({ user }, secretKey);
-        return res.json({ sucess: true, token: token, admin: user.admin });
+        return res.json({ success: true, token: token, admin: user.admin });
     } catch (error) {
-        return res.status(500).json({ message: error.message });
+        return res.status(500).json({ success: false, message: error.message });
     }
 });
 
---
+++ fe/src/pages/Login.jsx
@@ -0,0 +1,50 @@
+import './Login.css'
+import { useState } from 'react'
+import axios from 'axios'
+const Login = () => {
+
+    const login_URL = 'http://127.0.0.1:8000/auth/login'
+    const [username, setUsername] = useState('')
+    const [password, setPassword] = useState('')
+    const login = async () => {
+        try {
+            const response = await axios.post(login_URL, {
+                Username: username,
+                Password: password
+            })
+            console.log("response: ", response)
+            console.log("response.data: ", response.data)
+            if (response.data.success) {
+                console.log("Login successful", "success: ", response.data.success, "token: ", response.data.token, "admin: ", response.data.admin)
+                localStorage.setItem('token', response.data.token)
+                localStorage.setItem('admin', response.data.admin)
+            } else {
+                console.log("Login failed: ", "error: ", response.data.error, "success: ", response.data.success)
+            }
+        } catch (error) {
+            if (error.response) {
+                console.error("success: ", error.response.data.success, "error: ", error.response.data.error)
+                alert(error.response.data.error)
+            } else if (error.request) {
+                console.error("error: ", error.request)
+                alert(error.request)
+            }
+        }
+    }
+
+    return (
+        <div className='Login'>
+            <label className="heading">Next Generation E-Commerce</label>
+            <br />
+            <label>Username:</label>
+            <input type="text" onChange={e => setUsername(e.target.value)} /><br />
+            <label>Password:</label>
+            <input type="password" onChange={e => setPassword(e.target.value)} /><br />
+            <button className='LoginButton' onClick={login}>Login</button>
+            <label className="newUser"> New User?
+                <a className="register" href="/register" > Register</a>
+            </label>
+        </div>
+    )
+}
+export default Login
