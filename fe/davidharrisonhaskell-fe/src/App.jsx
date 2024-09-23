1b400189f8558bee4f88cd3a45456d42cd73909b
David Harrison Haskell <david.h.haskell@gmail.com>
2024-09-23 12:34:48 +0300

    changes

--- fe/davidharrisonhaskell-fe/src/App.jsx
+++ fe/davidharrisonhaskell-fe/src/App.jsx
@@ -1,10 +1,11 @@
-
+import 'bootstrap/dist/css/bootstrap.min.css'
 
 function App() {
 
   return (
     <>
       <h1>E-commerce Website</h1>
+      Hello <input type="text" placeholder="Enter your name" />
     </>
   )
 }
